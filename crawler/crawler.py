d1bea7ee5e7bc1941aea3f4698efaa7b1e865d81
Duong Nguyen Anh Khoa <dnanhkhoa@live.com>
2017-01-31 12:00:44 +0700

    Update

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-import logging
-
 import regex
 
 from crawler.parser import BaseParser
-from utils import create_parser_from_files
+from helpers import create_parser_from_files
 
 
 class Crawler(object):
     _domain_regex = regex.compile(r'^https?://(?:www\.)?(\w+(?:\.\w+)+)', regex.IGNORECASE)
 
     def __init__(self):
-        self._logger = logging.getLogger(__name__)
         self.parser = create_parser_from_files('crawler/parser/subclasses', BaseParser)
 
     def crawl(self, url, date=None, timeout=15):
-        result = self._domain_regex.search(url)
-        if result is None:
+        matcher = self._domain_regex.search(url)
+        if matcher is None:
             raise Exception('URL không hợp lệ: %s' % url)
-        domain = result.group(1)
+        domain = matcher.group(1)
         if domain not in self.parser:
-            raise Exception('Không có parser nào hỗ trợ URL: %s' % url)
+            raise Exception('Tên miền %s chưa được hỗ trợ.' % domain)
         return self.parser[domain].parse(url=url, date=date, timeout=timeout)
--- normalizer/normalizer.py
+++ normalizer/normalizer.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-import logging
-
 import regex
 
+from helpers import create_parser_from_files
 from normalizer.parser import BaseParser
-from utils import create_parser_from_files
 
 
 class Normalizer(object):
     _domain_regex = regex.compile(r'^https?://(?:www\.)?(\w+(?:\.\w+)+)', regex.IGNORECASE)
 
     def __init__(self):
-        self._logger = logging.getLogger(__name__)
         self.parser = create_parser_from_files('normalizer/parser/subclasses', BaseParser)
 
     def normalize(self, url, timeout=15):
-        result = self._domain_regex.search(url)
-        if result is None:
+        matcher = self._domain_regex.search(url)
+        if matcher is None:
             raise Exception('URL không hợp lệ: %s' % url)
-        domain = result.group(1)
+        domain = matcher.group(1)
         if domain not in self.parser:
-            raise Exception('Không có parser nào hỗ trợ URL: %s' % url)
+            raise Exception('Tên miền %s chưa được hỗ trợ.' % domain)
         return self.parser[domain].parse(url=url, timeout=timeout)
